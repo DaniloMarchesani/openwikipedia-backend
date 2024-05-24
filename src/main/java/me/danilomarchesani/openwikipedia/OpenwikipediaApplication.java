5b23aa5124569b1e6636b8bf01652dd0d74b0514
Danilo Marchesani <marchesanidanilo@gmail.com>
2024-05-24 22:09:54 +0200

    first commit

---
+++ src/main/java/me/danilomarchesani/openwikipedia/OpenwikipediaApplication.java
@@ -0,0 +1,13 @@
+package me.danilomarchesani.openwikipedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenwikipediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenwikipediaApplication.class, args);
+	}
+
+}
